c63243426c5d2e7b8d81100e191cd5f5a00b136d
Gulshan Yadav <63960580+yadavgulshan@users.noreply.github.com>
2020-11-25 17:47:10 +0530

    Add files via upload

---
+++ irctc.py
@@ -0,0 +1,91 @@
+# from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+# import pyautogui as pg
+from time import sleep
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
+import pyautogui as pg
+driver = webdriver.Chrome(ChromeDriverManager(
+    chrome_type=ChromeType.CHROMIUM).install())
+# from selenium import webdriver
+
+# from webdriver_manager.chrome import GeckoDriverManager
+# options = webdriver.FirefoxOptions()
+
+
+# options.add_argument("--headless")
+
+# driver = webdriver.Chrome(
+# ChromeDriverManager().install(), options=options)
+
+# driver = webdriver.Firefox(executable_path='/home/gulshan/geckodriver')
+ok_xpath = '/html/body/app-root/app-home/div[1]/app-header/p-dialog[2]/div/div[2]/div/form/div[2]/button'
+from_xpath = '//*[@id="origin"]/span/input'
+to_xpath = '//*[@id="destination"]'
+date_xpath = '//*[@id="divMain"]/div/app-main-page/div[1]/div/div[1]/div/div/div[1]/div/app-jp-input/div[3]/form/div[3]/p-calendar/span/input'
+class_xpth = '//*[@id="journeyClass"]/div/label'
+train_xpath = '//*[@id="divMain"]/div/app-main-page/div[1]/div/div[1]/div/div/div[1]/div/app-jp-input/div[3]/form/div[7]/button'
+fromStation = 'THANE - TNA'
+toStation = 'JAKHANIAN - JKN'
+dateOfJourney = '26-11-2020'
+noOfPassengers = 3
+noOfPassengers_xpath = '//*[@id="numberOfPassengers"]/div/label'
+modifySearch_xpath = '//*[@id="ui-accordiontab-0-content"]/div/div/form/div[1]/div[6]/button'
+username = 'Gulshan541'
+password = 'Yadavgulshan541'
+driver.get("https://www.irctc.co.in/nget/train-search")
+assert "IRCTC" in driver.title
+ok = driver.find_element_by_xpath(ok_xpath)
+ok.click()
+
+
+def firstpage():
+
+    from_station = driver.find_element_by_xpath(from_xpath)
+    from_station.clear()
+    from_station.send_keys(fromStation)
+
+    # pg.write(fromStation)
+    # pg.press("enter")
+
+    pg.press("tab")
+    pg.press("tab")
+
+    pg.write(toStation)
+    pg.press("tab")
+    pg.press("backspace")
+    pg.write(dateOfJourney)
+    # pg.press("enter")
+    pg.press("tab")
+
+    for i in range(0, 9):
+        pg.press("down")
+    # classCoach = driver.find_element_by_xpath(class_xpth)
+    # classCoach.click()
+    # classCoach.click()
+    pg.press("tab")
+    pg.press("tab")
+    pg.press("tab")
+    pg.press("enter")
+
+
+def booknow():
+    username_xpath = driver.find_element_by_xpath('//*[@id="userId"]')
+    username_xpath.send_keys(username, Keys.RETURN)
+    password_xpath = driver.find_element_by_xpath('//*[@id="pwd"]')
+    password_xpath.send_keys(password, Keys.RETURN)
+
+    sleep(10)
+    print("sleep ended...............................................................")
+
+
+# to_station = driver.find_element_by_xpath(to_xpath)
+# to_station.clear()
+# to_station.send_keys(toStation)
+# pg.press("enter")
+firstpage()
+sleep(15)
+booknow()
+sleep(10)
+# driver.close()
